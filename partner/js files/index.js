7084b54d298f6aaa259d404e722d001cbac250a7
gsandoo <rogersddt@naver.com>
2022-07-23 19:27:16 +0900

    feat: 파트너매칭 메인페이지 api 연결

--- partner/js files/index.js
+++ partner/js files/index.js
@@ -1,11 +1,30 @@
 $(function(){
-    //입력박스 숨어있다가
-$(".search-input").hide();
+    
 $(".search-toggle").change(function() {
               //기타를 선택하면 등장
-      if($(".search-toggle").val() == "etc") {
-          $(".search-input").show();
-          $(".search-toggle").hide();
+      if($(".search-toggle").val() == "etc") {     
+          $(".search-box").css("display","none");
+          $(".search-box-input").css("display","flex");
       }  
   }) 
 });
+
+// 검색
+
+const query = document.querySelector('.query');
+
+
+function onQuerySelected(){
+    const querySelect = document.querySelector('.search-toggle');
+    const query = querySelect.options[querySelect.selectedIndex].value;
+    console.log(query);
+    if(query == "etc"){
+        const inputValue = document.querySelector('.search-input');
+        
+    }
+    
+};
+
+function onChange(e){
+   console.log(e.value);
+};
